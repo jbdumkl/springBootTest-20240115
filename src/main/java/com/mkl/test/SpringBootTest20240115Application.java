abc5024f8a0ca7f1efb2a96867efdc49b5e4cb95
jbdumkl <gglmkl@gmail.com>
2024-01-15 17:21:25 +0900

    springBootTest-240115

---
+++ src/main/java/com/mkl/test/SpringBootTest20240115Application.java
@@ -0,0 +1,13 @@
+package com.mkl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest20240115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest20240115Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkl/test/SpringBootTest20240115ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest20240115ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
